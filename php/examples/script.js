700d36a4bf57b9901372237f279845eda78a3b35
av.novikov3 <av.novikov3@tensor.ru>
2019-04-12 09:59:36 +0300

    add jquery error handling

--- php/examples/script.js
+++ php/examples/script.js
@@ -4,15 +4,23 @@ $(document).ready(function(){
 	 latency = $('input[name="latency"]').val();
 	 $.ajax({
 		url: "server.php?latency=" + latency,
-		beforeSend: function( xhr ) {
+		beforeSend: function(xhr) {
 			console.log('before');
 			$('#ajax_loader').html('<img src="loading.gif" alt="Loading" height="42" width="42">');
+		},
+		error: function(jqXHR, textStatus, errorThrown) {
+			console.log("error\njqXHR: " + JSON.stringify(jqXHR) + 
+			            "\ntextStatus: " + textStatus +
+						"\nerrorThrown: " + errorThrown);
+			$('#ajax_loader').html('<p style="color: red; margin: 0; padding: 0;">Ошибка выполнения!!!</p>');
 		}
 	 })
 	 .done(function( data ) {
 		 console.log('done');
 		 console.dir(data);
-		 $('#ajax_loader').text('Время задержки: ' + data.sleep + ' (сек). Click me again!');
+		 $('#ajax_loader').html('<p style="color: green; margin: 0; padding: 0;">'+
+		                         'Время задержки: ' + data.sleep + ' (сек). Click me again!'+
+								'</p>');
       });	
 	 console.log('Finish');
     });
